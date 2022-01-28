e61089c3ccf97fd7e0aa4b487ca5163bba26c2b1
Erkki Seppälä <erkki.seppala@vincit.fi>
2022-01-28 22:10:57 +0200

    check.sh: do dot output only with the switch -dot

--- check.sh
+++ check.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-set -x
-tlc -workers auto -dump dot,colorize,actionlabels System.dot System
+#!/bin/zsh
+typeset -a flags=()
+if [ "$1" = "-dot" ]; then
+    shift
+    flags=($flags -dump dot,colorize,actionlabels System.dot)
+fi
+tlc -workers auto ${(@)flags} System
