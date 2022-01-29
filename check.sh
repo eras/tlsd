3d328b7c55d0f88a976aade2e2e326c9b6e0e0df
Erkki Seppälä <erkki.seppala@vincit.fi>
2022-01-29 12:47:58 +0200

    check.sh: added more flags and integrated dot-support

--- check.sh
+++ check.sh
@@ -1,7 +1,49 @@
 #!/bin/zsh
-typeset -a flags=()
-if [ "$1" = "-dot" ]; then
+typeset -a tlc_args=()
+typeset -a dot_args=()
+
+module=System
+
+tlc_arg() {
+    tlc_args=($tlc_args $@)
+}
+
+dot_arg() {
+    dot_args=($dot_args $@)
+}
+
+dot=false
+
+tlc_arg -workers auto
+tlc_arg -fpmem 0.1
+
+while [ -n "$1" ]; do
+    arg="$1"
     shift
-    flags=($flags -dump dot,colorize,actionlabels System.dot)
+    case "$arg"; in
+	-dot)
+	    dot=true
+	    tlc_arg -dump dot,colorize,actionlabels "$module".dot
+	    ;;
+	-diff)
+	    tlc_arg -difftrace
+	    ;;
+	-lr)
+	    dot_arg -lr
+	    ;;
+	*)
+	    echo unknown argument "$arg"
+	    exit 2
+    esac
+done
+
+echo_and_run() {
+    echo "$*"
+    "$@"
+}
+
+echo_and_run tlc ${(@)tlc_args} "$module"
+if $dot; then
+    (ulimit -v 1000000;
+     echo_and_run dot-tla-model ${(@)dot_args} "$module".dot > "$module".pdf)
 fi
-tlc -workers auto ${(@)flags} System
